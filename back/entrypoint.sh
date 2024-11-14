85ad9cf8aac4d47af4ac1c974221c62427040ac9
LiviaMartinss <lilivitt001@gmail.com>
2024-11-14 01:26:54 -0300

    .

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export RAILS_MASTER_KEY=seu_master_key_aqui
 
 # Espera até que o banco de dados esteja pronto
 until (echo > /dev/tcp/db/5432) >/dev/null 2>&1
@@ -12,3 +13,15 @@ rails db:migrate RAILS_ENV=development
 
 # Inicializa o servidor Rails
 rails server -b 0.0.0.0
+
+
+### Ver se comentando esse codigo ele para de dar erro
+# Ver se troc por isso 
+#!/bin/bash
+# set -e
+
+#remove potentially pe-ex server.pid for rails
+# rm -f /myapp/tmp/pids/server.pid
+
+# then exec
+#exec "$@"
