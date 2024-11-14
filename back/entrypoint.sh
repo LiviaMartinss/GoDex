be27a7160c7c7f7e758ae91dc150a6434c6a765e
LiviaMartinss <lilivitt001@gmail.com>
2024-11-14 12:06:54 -0300

    número máximo de tentativas bd e mudanças no front

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 export RAILS_MASTER_KEY=seu_master_key_aqui
+#!/bin/bash
 
-# Espera até que o banco de dados esteja pronto
-until (echo > /dev/tcp/db/5432) >/dev/null 2>&1
-do
-  echo "Aguardando o banco de dados..."
+# Configurar número máximo de tentativas e intervalo entre elas
+MAX_RETRIES=30
+RETRY_COUNT=0
+
+echo "Aguardando o banco de dados..."
+
+# Tentar conectar ao banco de dados
+until nc -z -v -w30 db 5432 || [ $RETRY_COUNT -eq $MAX_RETRIES ]; do
+  echo "Tentativa de conexão ao banco de dados ($RETRY_COUNT/$MAX_RETRIES)"
   sleep 1
+  RETRY_COUNT=$((RETRY_COUNT+1))
 done
 
-# Executa as migrações pendentes
-rails db:migrate RAILS_ENV=development
+if [ $RETRY_COUNT -eq $MAX_RETRIES ]; then
+  echo "Não foi possível conectar ao banco de dados após várias tentativas."
+  exit 1
+fi
 
-# Inicializa o servidor Rails
+echo "Banco de dados conectado com sucesso!"
+
+# Executar as migrações e iniciar o servidor
+rails db:migrate RAILS_ENV=production
 rails server -b 0.0.0.0
-
-
-### Ver se comentando esse codigo ele para de dar erro
-# Ver se troc por isso 
-#!/bin/bash
-# set -e
-
-#remove potentially pe-ex server.pid for rails
-# rm -f /myapp/tmp/pids/server.pid
-
-# then exec
-#exec "$@"
